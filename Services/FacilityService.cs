c9fd7aa4455c67b76e62de8c4a5518908cbc8e47
hieunn-dev <nguyenngochieu0323@gmail.com>
2025-12-18 14:33:00 +0700

    update

--- Services/FacilityService.cs
+++ Services/FacilityService.cs
@@ -16,11 +16,12 @@ namespace FPT_Booking_BE.Services
             _context = context;
         }
 
-        public async Task<List<FacilityDto>> GetAllFacilities(string? name, int? campusId, int? typeId)
+        public async Task<List<FacilityDto>> GetAllFacilities(string? name, int? campusId, int? typeId, int? slotId, DateOnly? date)
         {
             var query = _context.Facilities
                 .Include(f => f.Campus)
                 .Include(f => f.Type)
+                .Where(f => f.Status == "Available")
                 .AsQueryable();
 
             if (!string.IsNullOrEmpty(name))
@@ -36,6 +37,16 @@ namespace FPT_Booking_BE.Services
             if (typeId.HasValue)
             {
                 query = query.Where(f => f.TypeId == typeId);
+            }
+
+            if (slotId.HasValue && date.HasValue)
+            {
+                query = query.Where(f => !_context.Bookings.Any(b =>
+                    b.FacilityId == f.FacilityId &&
+                    b.BookingDate == date &&
+                    b.SlotId == slotId &&
+                    b.Status == "Approved"
+                ));
             }
 
             return await query
